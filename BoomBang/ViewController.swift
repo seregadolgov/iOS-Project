e9e4656e823361e1cd97df244585221ef68d56af
Sergey Dolgov <Sergey@MacBook-Air-Sergey.local>
2016-06-25 23:06:54 -0700

    Added Files Boom Bang

--- BoomBang/ViewController.swift
+++ BoomBang/ViewController.swift
@@ -10,6 +10,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var HideRedButton: UIButton!
+    @IBOutlet weak var HideBlueButton: UIButton!
+    @IBOutlet weak var RedCat: UIImageView!
+    @IBOutlet weak var BlueCat: UIImageView!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +27,13 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func HideRedButtonPressed(sender: AnyObject){
+        RedCat.hidden = true
+    }
+    
+    @IBAction func HideBlueButtonPressed(sender: AnyObject) {
+        BlueCat.hidden = true
+    }
 
 }
 
